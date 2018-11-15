d4ae19b719a84ae435117c1cc1ece7e202c44fe9
AndreyU76 <43966723+AndreyU76@users.noreply.github.com>
2018-11-15 15:26:34 +0300

    Create test.client.py

---
+++ test.client.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+def name(account_name, test):
+    return()
+
+class Testname(unittest.TestCase):
+    def testname(self):
+        self.assertIn(name(account_name), test)
+
+if __name__== "__main__":
+    unittest.main()
