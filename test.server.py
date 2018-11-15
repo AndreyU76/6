d9046b20bbcbb94b80001b4cac0207ae80ff851a
AndreyU76 <43966723+AndreyU76@users.noreply.github.com>
2018-11-15 15:26:52 +0300

    Update test.server.py

--- test.server.py
+++ test.server.py
@@ -1,15 +1,10 @@
 import unittest
-
-def account_name():
-    pass
-def name():
-    pass
-def split(account_name, test):
-    return()
-
+import serv
 class Testname(unittest.TestCase):
-    def testname(self):
-        self.assertIsNot(name(account_name), test)
+    def test_authenticate(self):
+        self.assertEqual(request(account_name, test))
+
+   
 
 if __name__== "__main__":
     unittest.main()
