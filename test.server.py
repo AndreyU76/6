5b8daa8489cc132bebebce6ff6c535b73cdf78e5
AndreyU76 <43966723+AndreyU76@users.noreply.github.com>
2018-11-15 14:31:08 +0300

    Create test.server.py

---
+++ test.server.py
@@ -0,0 +1,15 @@
+import unittest
+
+def account_name():
+    pass
+def name():
+    pass
+def split(account_name, test):
+    return()
+
+class Testname(unittest.TestCase):
+    def testname(self):
+        self.assertIsNot(name(account_name), test)
+
+if __name__== "__main__":
+    unittest.main()
