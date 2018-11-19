bcd0725457d4cd8d4a40186c59b3599cbb3d2d2d
AndreyU76 <43966723+AndreyU76@users.noreply.github.com>
2018-11-19 13:14:19 +0300

    Update client_log_config.py

--- client_log_config.py
+++ client_log_config.py
@@ -1,11 +1,13 @@
 import logging
 import sys
 
+
 logging.basicConfig(
     filename = "client.log",
     format="%(levelname)-10s %(asctime)s %(message)s",
     level=logging.DEBUG
 )
+
 
 logger = logging.getLogger(__name__)
 logger.critical('DEBUG')
